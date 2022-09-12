a3d4e29a8e6b7b4e6a4eb8783cd20a4c84bde2dd
Jason Koh <jakoh@u91z06s04.42kl.edu.my>
2022-09-12 18:31:50 +0800

    done dollar expansion and quotes

--- srcs/expand.c
+++ srcs/expand.c
@@ -6,7 +6,7 @@
 /*   By: jakoh <jakoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 18:03:29 by jakoh             #+#    #+#             */
-/*   Updated: 2022/09/12 17:29:57 by jakoh            ###   ########.fr       */
+/*   Updated: 2022/09/12 18:31:31 by jakoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 // expanding arguments
 // just like tokenizing
 // only this time we free cur_node->val and string join the new part of the string
-
 void	exspender(t_main *m_var, t_node **cur_node);
 void	spender(t_main *m_var, t_node **lists)
 {
@@ -29,6 +28,7 @@ void	spender(t_main *m_var, t_node **lists)
 	}
 }
 
+char	*ft_monies(t_main *m_var,char *s1, char *s2);
 char	*sjoin_ext(char *s1, char *s2);
 void	exspender(t_main *m_var, t_node **cur_node)
 {
@@ -36,8 +36,10 @@ void	exspender(t_main *m_var, t_node **cur_node)
 	t_toke_var	s;
 	char		*str;
 	char		*temp;
-	char		*temp2;
 
+	(void)m_var;
+	str = NULL;
+	temp = NULL;
 	node = *cur_node;
 	if (node->type != 2 || node->val == NULL)
 		return ;
@@ -54,16 +56,17 @@ void	exspender(t_main *m_var, t_node **cur_node)
 				str = sjoin_ext(str, temp);
 			}
 			s.left = s.right;
-			++(s.left);
-			if (node->val[s.left] == '\'')
+			if (node->val[(s.left)] == '\'')
 			{
+				++(s.left);
 				while (node->val[++(s.right)] != '\'')
 					;
 				temp = ft_substr(node->val, s.left, s.right - s.left);
 				str = sjoin_ext(str, temp);
 			}
-			else if (node->val[s.left] == '\"')
+			else if (node->val[(s.left)] == '\"')
 			{
+				++(s.left);
 				while (node->val[++(s.right)] != '\"')
 				{
 					if (node->val[s.right] == '$')
@@ -78,10 +81,10 @@ void	exspender(t_main *m_var, t_node **cur_node)
 						while (is_op(node->val[++s.right]) != 9 && node->val[s.right] != '\"')
 							;
 						temp = ft_substr(node->val, s.left, s.right - s.left);
-						// str = 
+						str = ft_monies(m_var, str, temp);
+						s.left = s.right;
 					}
 				}
-				
 				temp = ft_substr(node->val, s.left, s.right - s.left);
 				str = sjoin_ext(str, temp);
 			}
@@ -90,6 +93,10 @@ void	exspender(t_main *m_var, t_node **cur_node)
 		else
 			++(s.right);
 	}
+	temp = ft_substr(node->val, s.left, s.right - s.left);
+	str = sjoin_ext(str, temp);
+	free(node->val);
+	node->val = str;
 }
 
 char	*sjoin_ext(char *s1, char *s2)
@@ -98,13 +105,28 @@ char	*sjoin_ext(char *s1, char *s2)
 	
 	if (s1 == NULL)
 		return (s2);
+	else if (s2 == NULL)
+		return (s1);
 	temp = ft_strjoin(s1, s2);
 	free(s1);
 	free(s2);
 	return (temp);
 }
 
-// char	*ft_monies(t_main *m_var,char *s1, char *s2);
-// {
-// 	char	*temp;
-// }
+char	*ft_monies(t_main *m_var,char *s1, char *s2)
+{
+	char	*temp;
+	int		i;
+
+	i = -1;
+	while (m_var->envp[++i] != NULL)
+		if (ft_strnstr(m_var->envp[i], s2, ft_strlen(s2)) != 0)
+			break;
+	if (m_var->envp[i] == NULL)
+		temp = ft_strdup("");
+	else
+		temp = ft_strdup(m_var->envp[i] + ft_strlen(s2) + 1);
+	temp = sjoin_ext(s1, temp);
+	free(s2);
+	return (temp);
+}
