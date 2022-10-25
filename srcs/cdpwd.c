e04165524dcd0d941b2f1736ee4ffcdea043b86e
PandieYay <babyedison@gmail.com>
2022-10-25 15:40:38 +0800

    Fixed segfault and can now cd ~ and cd

--- srcs/cdpwd.c
+++ srcs/cdpwd.c
@@ -6,7 +6,7 @@
 /*   By: edlim <edlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 20:51:49 by edlim             #+#    #+#             */
-/*   Updated: 2022/10/11 20:51:50 by edlim            ###   ########.fr       */
+/*   Updated: 2022/10/25 15:32:36 by edlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,24 @@ void	cdpwd(t_main *m_var, t_cmds **cmd_groups)
 
 	while ((*cmd_groups) != NULL)
 	{
+		if (!(*cmd_groups)->args[0])
+			return ;
 		if (ft_strcmp((*cmd_groups)->args[0], "cd") == 0)
 		{
-			// If pipe should do nothing
-			if ((*cmd_groups)->args[1] != NULL)
-			{
-				if (chdir((*cmd_groups)->args[1]) == -1)
-					printf("cd: no such file or directory: %s\n",
-						(*cmd_groups)->args[1]);
-			}
+	 		// If pipe should do nothing
+	 		if ((*cmd_groups)->args[1] != NULL)
+	 		{
+				if (ft_strcmp((*cmd_groups)->args[1], "~") == 0)
+					chdir(getenv("HOME"));			
+	 			else if (chdir((*cmd_groups)->args[1]) == -1)
+	 				printf("cd: no such file or directory: %s\n",
+	 					(*cmd_groups)->args[1]);
+	 		}
+			else
+				chdir(getenv("HOME"));
 		}
-		else if (ft_strcmp((*cmd_groups)->args[0], "pwd") == 0)
-		{
-			printf("%s\n", getcwd(cwd, sizeof(cwd)));
-		}
+	 	else if (ft_strcmp((*cmd_groups)->args[0], "pwd") == 0)
+	 		printf("%s\n", getcwd(cwd, sizeof(cwd)));
 		(*cmd_groups) = (*cmd_groups)->next;
 	}
 	(void)m_var;
